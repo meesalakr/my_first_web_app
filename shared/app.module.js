354ac7d7fd039f8220eb158f58723f62adcb3fa4
meesalakr <kotislr75@gmail.com>
2017-08-24 22:08:35 +0530

    Update app.module.js

--- shared/app.module.js
+++ shared/app.module.js
@@ -1,4 +1,4 @@
-var app=angular.module("myApp",["ui.router"]);
+var app=angular.module("myApp",["ui.router",'ui.bootstrap']);
 
 app.run(function($rootScope,$window,auth){
      auth.isloggedIn().then(function(response){
